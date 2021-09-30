b4b84313e04b8ea210133db3f5b8446a52c3257f
hmzsjd <github@hamzasajid.com>
2021-09-30 14:42:46 -0700

    initial commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,10 @@
+// commit comment
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
+  
 */
 
 /*
